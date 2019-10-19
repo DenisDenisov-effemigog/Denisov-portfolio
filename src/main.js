33a7e0fb0c364e8383a9c623c2569a718230937c
Denisov Denis <Effemigog@yandex.ru>
2019-10-19 19:28:20 +0300

    сломаный параллакс

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
-import "./scripts/skills"
-import "./styles/main.pcss";
+import "./styles/main.pcss" 
+import "./scripts/skills" 
+import "./scripts/reviewsSlaider" 
+
+
 if (process.env.NODE_ENV === "development") {
   require("file-loader!./index.pug");
 }

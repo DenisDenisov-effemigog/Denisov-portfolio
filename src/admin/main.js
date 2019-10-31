22d815915cb2a65f222b70b5e5670b36a4e4fe15
Denisov Denis <Effemigog@yandex.ru>
2019-10-31 18:45:49 +0300

    Настроил валидацию первой секции

--- src/admin/main.js
+++ src/admin/main.js
@@ -1,11 +1,13 @@
 import Vue from 'vue';
 import App from './App.vue'
 import SimpleVueValidation from 'simple-vue-validator';
+import router from './router'
 
 Vue.use(SimpleVueValidation);
 
 new Vue({
   el: "#app-root",
+  router,
   render: h => h(App),
   
   
